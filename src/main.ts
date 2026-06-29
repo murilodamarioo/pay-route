ec18354a32486786dcc7c4724719d0888ab043fd
Murilo Damario <murilo.gcdamario@gmail.com>
2026-06-29 19:31:16 -0300

    :recycle: refactor: Update Zookeeper healthcheck command and enhance main application bootstrapping

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './app.module'
+
+import { EnvService } from './config/env.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  const envService = app.get(EnvService)
+  const port = envService.get('PORT')
+
+  await app.listen(port)
 }
 bootstrap();
